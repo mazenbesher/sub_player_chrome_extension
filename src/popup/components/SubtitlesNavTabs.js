6c845be4a4c30a693fce1b00611abf4393dafdfd
mazen <mazenbesher@gmail.com>
2017-08-18 02:04:42 +0200

    custom scrollbar and classify some components

--- src/popup/components/SubtitlesNavTabs.js
+++ src/popup/components/SubtitlesNavTabs.js
@@ -1,25 +1,38 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+class SubtitleTab extends React.Component {
+    render() {
+        const { subId } = this.props;
+
+        return (
+            <a
+                className={"nav-item nav-link" + (subId == 1 ? " active" : " ")}
+                data-toggle="tab"
+                href={`#subtitle_${subId}`}
+                role="tab"
+                aria-controls={`subtitle_${subId}`}
+                aria-expanded={subId == 1 ? "true" : ""}>
+                Subtitle {subId}
+            </a >
+        )
+    }
+}
+
 // first one is active
 const listItems = [1, 2, 3].map(num =>
-    <a
-        key={num}
-        className={"nav-item nav-link" + (num == 1 ? " active" : "")}
-        data-toggle="tab"
-        href={`#subtitle_${num}`}
-        role="tab"
-        aria-controls={`subtitle_${num}`}
-        aria-expanded={num == 1 ? "true" : ""}>
-        Subtitle {num}
-    </a >
+    React.createElement(SubtitleTab, { key: num, subId: num })
 );
 
-export const SubtitlesNavTabs = () => (
-    <ul
-        id="subtitles_nav_tabs"
-        className="nav nav-tabs"
-        role="tablist">
-        {listItems}
-    </ul >
-)
+export class SubtitlesNavTabs extends React.Component {
+    render() {
+        return (
+            <ul
+                id="subtitles_nav_tabs"
+                className="nav nav-tabs"
+                role="tablist">
+                {listItems}
+            </ul >
+        )
+    }
+}
